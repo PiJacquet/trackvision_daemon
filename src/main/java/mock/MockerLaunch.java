8fdea23bfd06c66521e214e25bfbe0a50c9a8872
Oussama Bouachrine <oussama.bouachrine@etu.u-pec.fr>
2019-12-16 11:33:13 +0100

    Update

--- src/main/java/mock/MockerLaunch.java
+++ src/main/java/mock/MockerLaunch.java
@@ -3,7 +3,6 @@ package mock;
 public class MockerLaunch {
 
 	public static void main(String[] args) throws InterruptedException{
-		
 		//new Mocker();
 		new Thread(new HeartRateData()).start();
 		new Thread(new BloodSugarLevelData()).start();

fcfe16c273722f4d885195714c4678e990f24c4c
Oussama Bouachrine <oussama.bouachrine@etu.u-pec.fr>
2020-01-27 14:25:03 +0100

    add main class

---
+++ src/main/java/mock/MockerLaunch.java
@@ -0,0 +1,14 @@
+package mock;
+
+public class MockerLaunch {
+	
+
+		public static void main(String[] args) throws InterruptedException{
+			//new Mocker();
+			new Thread(new HeartRateData()).start();
+			//new Thread(new BloodSugarLevelData()).start();
+		}
+
+		
+
+}
